1c36ab5457d67315bd7ef4dc0cc6d5e852fc9802
上原僚馬 <ryoma@uehararyoubanoMacBook-Pro.local>
2020-09-12 05:42:26 +0900

    create  class components

--- src/App.js
+++ src/App.js
@@ -1,16 +1,31 @@
 import React from 'react';
 
-function App() {
+// function App() {
 
-  const GREETING = "Hi, Tom!"
-  const DOM = <input type="text" onChange={() => {console.log("I am clecked.")}} />
+//   const GREETING = "Hi, Tom!"
+//   const DOM = <input type="text" onChange={() => {console.log("I am clecked.")}} />
 
-  return (
-    <header>
-      <label htmlFrom="bar">bar</label>
-      {DOM}
-    </header>
-  );
+//   return (
+//     <header>
+//       <label htmlFrom="bar">bar</label>
+//       {DOM}
+//     </header>
+//   );
+// }
+
+const App = () => {
+  return <div>
+      Hi!
+      <Cat />
+      <Cat />
+      <Cat />
+      <Cat />
+    </div>
 }
+
+const Cat = () => {
+  return <div>Meaw!</div>
+}
+
 
 export default App;
