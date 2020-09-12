e23d388daf2650d26479aa71996a1802a7eed554
上原僚馬 <ryoma@uehararyoubanoMacBook-Pro.local>
2020-09-13 05:34:36 +0900

    learn store

--- src/index.js
+++ src/index.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+
 import './index.css';
-import App from './App';
+import reducer from './reducers'
+
+import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 
+const store = createStore(reducer)
+
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>
+  // <React.StrictMode>
+  // </React.StrictMode>,
   document.getElementById('root')
 );
 
